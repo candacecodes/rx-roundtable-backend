6bf717d225152db042be3b2250790859311b6370
candacecodes <candacehchoe@gmail.com>
2020-11-18 15:50:46 -0800

    password_digest added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,37 @@
 class UsersController < ApplicationController
 
+    def show
+        if user 
+            render json: user
+        else 
+            render json: { error: 'user could not be found' }
+        end
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user 
+        else 
+            render json: { error: 'user could not be created' }
+        end
+    end
+
+    def update
+        user.update(user_params)
+        if user.valid?
+            render json: user
+        else 
+            render json: { error: 'user could not be found' }
+        end
+    end
+
+    def destroy
+        if user
+            user.delete
+        else
+            render json: { error: 'user could not be found' }
+        end
+    end
+    
 end
