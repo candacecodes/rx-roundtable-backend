626b882887e78e0df07384a1a43e79d810c16f76
candacecodes <candacehchoe@gmail.com>
2020-11-18 16:09:28 -0800

    schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#   create_table "users", force: :cascade do |t|
+#     t.string "username"
+#     t.string "password_digest"
+
+#   create_table "rxes", force: :cascade do |t|
+#     t.string "name"
+
+#   create_table "comments", force: :cascade do |t|
+#     t.string "description"
+#     t.integer "user_id"
+#     t.integer "rx_id"
+
+#   create_table "selectedrxes", force: :cascade do |t|
+#     t.integer "user_id"
+#     t.integer "rx_id"
+
+
+
