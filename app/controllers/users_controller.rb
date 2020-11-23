e7e4e3e05750fba95a163486d3e8f954d0988e54
candacecodes <candacehchoe@gmail.com>
2020-11-22 19:53:08 -0800

    added user login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,18 @@ skip_before_action :authorized, only: [:create]
         end
       end
 
+# LOGGING IN
+  def login
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
+    end
+  end
+
     def update
         user.update(user_params)
         if user.valid?
